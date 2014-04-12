68dc8a750b0e32a6726b3a57dfec65303c7bae89
Tuesday <tuesday@skLiMnk.(none)>
2014-04-12 15:45:05 +0800

    add rewites rules for app.js

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
 var express = require( 'express' );
 var app     = express();
 var type    = process.argv[2];
+var conf    = require( './config.json' );
+
+app.use( function( req, res, next ) {
+
+	var k, rule, redirect, reserved, displaced;
+
+	for ( k in conf.rewrites ) {
+
+		redirect = conf.rewrites[k];
+
+		try {
+
+			reserved = req.url.match( new RegExp( k ) );
+			displaced = redirect.match( /\$\d/g );
+
+			if ( !reserved ) throw '0';
+
+			if ( !displaced ) {
+
+				req.url = redirect;
+				next();
+			}
+
+			else {
+
+				displaced.forEach( function( value ) {
+
+					var index = value.replace( '$', '' );
+
+					if ( reserved[ index ] ) {
+
+						redirect = redirect.replace( value,  reserved[ index ] );
+					}
+
+					else {
+
+						throw '0';
+					}
+				});
+
+				req.url = redirect;
+				next();
+			}
+		}
+		catch( e ) {
+
+			continue;
+		}
+	}
+});
 
 if ( type == 'src' ) {
 
